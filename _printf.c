5bd665daf708d13c0bd62583002220b702da84f8
dannycod3r <oseid7807@gmail.com>
2023-03-27 08:57:04 +0000

    Implement function to print c, s, %

---
+++ _printf.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+int _printf(const char *format, ...)
+{
+	int count = 0, i = 0;
+	va_list args;
+	int (*func)(va_list);
+
+	va_start(args, format);
+
+	if (format == NULL)
+		return (-1);
+
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			func = get_func(format[i]);
+			if (func == NULL)
+			{
+				_putchar('%');
+				_putchar(format[i]);
+				count++;
+			}
+			else
+			{
+				count = func(args);
+			}
+		}
+		else
+		{
+			_putchar(format[i]);
+			count++;
+		}
+		i++;
+	}
+
+	va_end(args);
+	return (count);
+}
