9f503efb18856701e333bb021b4ec348c70cd5d6
SelPerfect25 <selaperfect@gmail.com>
2023-03-27 10:34:11 -0700

    binary.c

--- binary.c
+++ binary.c
@@ -1,42 +1,45 @@
-#include <main.h>
+#include "main.h"
 /**
- * print_binary - Prints an unsigned number
- * @types: Lista of arguments
- * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
- * @width: get width.
- * @precision: Precision specification
- * @size: Size specifier
- * Return: Numbers of char printed.
+ * printf_unsigned - prints integer
+ * @args: argument to print
+ * Return: number of characters printed
  */
-int print_binary(va_list types, char buffer[],
-int flags, int width, int precision, int size)
+int printf_unsigned(va_list args)
 {
-unsigned int n, m, i, sum;
-unsigned int a[32];
-int count;
-UNUSED(buffer);
-UNUSED(flags);
-UNUSED(width);
-UNUSED(precision);
-UNUSED(size);
-n = va_arg(types, unsigned int);
-m = 2147483648; /* (2 ^ 31) */
-a[0] = n / m;
-for (i = 1; i < 32; i++)
-{
-m /= 2;
-a[i] = (n / m) % 2;
+        unsigned int n = va_arg(args, unsigned int);
+        int num, last = n % 10, digit, exp = 1;
+        int  i = 1;
+
+        n = n / 10;
+        num = n;
+
+        if (last < 0)
+        {
+                _putchar('-');
+                num = -num;
+                n = -n;
+                last = -last;
+                i++;
+        }
+        if (num > 0)
+        {
+                while (num / 10 != 0)
+                {
+                        exp = exp * 10;
+                        num = num / 10;
+                }
+                num = n;
+                while (exp > 0)
+                {
+                        digit = num / exp;
+                        _putchar(digit + '0');
+                        num = num - (digit * exp);
+                        exp = exp / 10;
+                        i++;
+                }
+        }
+        _putchar(last + '0');
+
+        return (i);
 }
-for (i = 0, sum = 0, count = 0; i < 32; i++)
-{
-sum += a[i];
-if (sum || i == 31)
-{
-char z = '0' + a[i];
-write(1, &z, 1);
-count++;
-}
-}
-return (count);
-}
+
