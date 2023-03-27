a9e9c514bd5672be3cb1aa24b63d4120f8d84abe
SelPerfect25 <selaperfect@gmail.com>
2023-03-27 09:18:26 -0700

    binary.c

--- binary.c
+++ binary.c
@@ -1,36 +1,34 @@
 #include "main.h"
 
 /**
- * binary_recursive - numbers in binary in recursive
- * @num: number to print
- * @len: lenght of digit to print
- *
- * Return: length on success, -1 if NULL
+ * printf_bin - prints a binary number.
+ * @val: arguments.
+ * Return: 1.
  */
-
-int binary_recursive(unsigned int num, int len)
+int printf_bin(va_list val)
 {
-	if (num / 2)
-		len = binary_recursive(num / 2, len + 1);
-	_putchar(num % 2 + '0');
+	int flag = 0;
+	int cont = 0;
+	int i, a = 1, b;
+	unsigned int num = va_arg(val, unsigned int);
+	unsigned int p;
 
-	return (len);
+	for (i = 0; i < 32; i++)
+	{
+		p = ((a << (31 - i)) & num);
+		if (p >> (31 - i))
+			flag = 1;
+		if (flag)
+		{
+			b = p >> (31 - i);
+			_putchar(b + 48);
+			cont++;
+		}
+	}
+	if (cont == 0)
+	{
+		cont++;
+		_putchar('0');
+	}
+	return (cont);
 }
-
-/**
- * print_binary - convert unsigned int to binary
- * @parameters: int to convert
- *
- * Return: integer
- */
-
-int print_binary(va_list parameters)
-{
-	int len = 0;
-
-	unsigned int num = va_arg(parameters, unsigned int);
-
-	len = binary_recursive(num, len) + 1;
-	return (len);
-}
-
