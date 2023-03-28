616ff270309eb86e555582eada1717f0a98d41e9
dannycod3r <oseid7807@gmail.com>
2023-03-28 23:53:23 +0000

    flags checking

--- _printf.c
+++ _printf.c
@@ -26,6 +26,20 @@ int _printf(const char *format, ...)
 
 			if (!(format[i]))
 				return (-1);
+
+			/* check for flags*/
+			while (format[i] == '+' || format[i] == ' ' || format[i] == '#')
+			{
+				if (format[i] == '+')
+					flags.plus = 1;
+				else if (format[i] == ' ')
+					flags.space = 1;
+				else if (format[i] == '#')
+					flags.hash = 1;
+
+				i++;
+			}
+
 			func = get_func(format[i]);
 			if (func == NULL)
 			{
